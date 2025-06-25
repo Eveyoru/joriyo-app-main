c1357594439c078c65c45b71acb4bb5317b57096
Eveyoru <sushantraut178@gmail.com>
2025-06-25 16:04:26 +0545

    Add new dependencies and update Metro config for polyfills
    Added several new dependencies including expo-linear-gradient, expo-secure-store, react-native-reanimated-carousel, react-native-url-polyfill, tsc, and web-streams-polyfill to package.json. Updated metro.config.js to alias web-streams-polyfill/ponyfill/es6 to es2018 and explicitly set supported platforms. These changes improve compatibility and add new features for the Expo client.

--- clientexpo/metro.config.js
+++ clientexpo/metro.config.js
@@ -5,7 +5,18 @@ const path = require('path');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration for web-streams-polyfill
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'web-streams-polyfill/ponyfill/es6': 'web-streams-polyfill/ponyfill/es2018'
+};
 
-
+// Add platform extensions
+config.resolver.platforms = [
+  'native',
+  'android',
+  'ios',
+  'web'
+];
 
 module.exports = config;
